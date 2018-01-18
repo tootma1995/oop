1cfca04bc8caf44f9a372c572a5ab2f324ff31c8
tootma1995 <norman.kukk@khk.ee>
2018-01-18 09:42:33 +0200

    conf.php abikonstandid

---
+++ conf.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Norman
+ * Date: 18/01/2018
+ * Time: 09:35
+ */
+
+// konfig fail
+
+// loome vajalikud abikonstandid
+define('MODEL_DIR', 'model/');
+define('VIEWS_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
