c2c814aba2608c3def41ecbe1a9e550ef884341d
tootma1995 <norman.kukk@khk.ee>
2018-01-18 10:30:18 +0200

    Template classi omaduste määramine

---
+++ Model/template.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Norman
+ * Date: 18/01/2018
+ * Time: 10:22
+ */
+
+class template
+{
+    // omadused
+    var $file = '';
+    var $content = false;
+    var $vars = array();
+}
