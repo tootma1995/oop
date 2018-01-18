b20079fb2490a3732a7a34f50692ee750313b0a3
tootma1995 <norman.kukk@khk.ee>
2018-01-18 11:24:30 +0200

    kasutusele võetud faili töötlus klass

--- conf.php
+++ conf.php
@@ -12,3 +12,6 @@
 define('MODEL_DIR', 'model/');
 define('VIEWS_DIR', 'views/');
 define('CONTROL_DIR', 'controllers/');
+
+// nõuame vajalike failide olemasolu
+require_once MODEL_DIR.'template.php'; //html vaade failide töötlus
