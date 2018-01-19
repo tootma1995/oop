aa609200c4f2ac4fdf96c69105d545d2546ee415
tootma1995 <norman.kukk@khk.ee>
2018-01-19 14:48:54 +0200

    reaalsed väärtused määratud

--- index.php
+++ index.php
@@ -11,6 +11,15 @@ require_once 'conf.php';
 
 $mainTmpl = new template('main');
 
+// reaalväärtuste määramine
+$mainTmpl->set('site_lang','et');
+$mainTmpl->set('site_title','PV');
+$mainTmpl->set('user','Kasutaja');
+$mainTmpl->set('title','Pealkiri');
+$mainTmpl->set('lang_bar','Keeleriba');
+$mainTmpl->set('menu','Menüü');
+$mainTmpl->set('content','Lehe sisu');
+
 echo '<pre>';
 print_r($mainTmpl);
 echo '</pre>';
