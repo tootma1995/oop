7d1e759efe789726009fc3626219810349a422fe
tootma1995 <norman.kukk@khk.ee>
2018-01-23 14:14:21 +0200

    tehtud ettevalmistus lehe loomiseks

--- index.php
+++ index.php
@@ -17,7 +17,8 @@ $mainTmpl->set('site_title','PV');
 $mainTmpl->set('user','Kasutaja');
 $mainTmpl->set('title','Pealkiri');
 $mainTmpl->set('lang_bar','Keeleriba');
-$mainTmpl->set('menu','Menüü');
+// failist menu
+require_once 'menu.php';
 $mainTmpl->set('content','Lehe sisu');
 
 echo '<pre>';
---
+++ menu.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Norman
+ * Date: 23/01/2018
+ * Time: 14:13
+ */
+
+
+$mainTmpl->set('menu','Lehe menu');
