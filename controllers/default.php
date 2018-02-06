41543bad709ecb3eebe1f23bf18d1a429134d3d8
tootma1995 <norman.kukk@khk.ee>
2018-02-06 13:13:00 +0200

    koostatud menu sisu db andmete pohjal, jamatud utf8ga

--- controllers/default.php
+++ controllers/default.php
@@ -6,5 +6,18 @@
  * Time: 09:00
  */
 
+$page_id= (int)$http->get('page_id'); // lehe id
+$sql = 'SELECT * FROM content '.
+    'WHERE content_id='.fixdb($page_id);
+$result = $db->getData($sql);
+if($result == false){
+    $sql = 'SELECT * FROM content '.
+        'WHERE is_first_page='.fixdb(1);
+    $result = $db ->getData($sql);
+}
+if($result != false){
+    $page = $result[0];
+    $http->set('page_id',$page['content_id']);
+    $mainTmpl->set('content', $page['content']);
+}
 
-$mainTmpl->set('content', 'Vaikimisi sisu');
--- doc/db-old.sql
+++ doc/db-old.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 06, 2018 at 12:46 PM
+-- Generation Time: Feb 05, 2018 at 11:18 AM
 -- Server version: 5.6.38
 -- PHP Version: 5.6.30
 
@@ -16,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `normanku_taasta`
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `content` (
   `content_id` bigint(20) UNSIGNED NOT NULL,
-  `content` mediumtext NOT NULL,
-  `clean_content` mediumtext NOT NULL,
-  `title` varchar(255) NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `clean_content` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `parent_id` bigint(20) UNSIGNED NOT NULL DEFAULT '0',
   `is_hidden` tinyint(1) NOT NULL DEFAULT '0',
   `show_in_menu` tinyint(1) NOT NULL DEFAULT '0',
@@ -40,12 +40,8 @@ CREATE TABLE `content` (
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `created` datetime NOT NULL,
   `is_first_page` tinyint(1) NOT NULL DEFAULT '0',
-  `lang_id` varchar(2) NOT NULL
+  `lang_id` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `content`
---
 
 --
 -- Indexes for dumped tables
@@ -65,7 +61,7 @@ ALTER TABLE `content`
 -- AUTO_INCREMENT for table `content`
 --
 ALTER TABLE `content`
-  MODIFY `content_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+  MODIFY `content_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
