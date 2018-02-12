0bce4b9d31eee38d6d9913d25f53d04185ca1943
tootma1995 <norman.kukk@khk.ee>
2018-02-12 12:31:04 +0200

    uus constant REMOTE_ADDR

--- model/http.php
+++ model/http.php
@@ -28,7 +28,7 @@ class http
     }
     // vajalike konstandite defineerimine
     function initConst(){
-        $constNames = array('HTTP_HOST', 'SCRIPT_NAME');
+        $constNames = array('HTTP_HOST', 'SCRIPT_NAME','REMOTE_ADDR');
         foreach ($constNames as $constName){
             if(!defined($constName) and isset($this->server[$constName])){
                 define($constName, $this->server[$constName]);
